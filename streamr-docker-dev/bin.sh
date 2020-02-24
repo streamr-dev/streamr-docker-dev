f3a0dc6bf3b6018a9b8e5bdbaea69368d500ab81
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-02-24 15:30:00 +0100

    2.0 (#58)
    * throw out stuff that gets easily outdated
    
    * fix dependencies
    
    * fix bug, add wipe command
    
    * minor cleanup
    
    * add wipe command to readme
    
    * fix readme heading levels
    
    * rename clean to factory-reset
    
    * fix nginx container name
    
    * remove unused flags from help
    
    * cleanup
    
    * remove unused variable
    
    * remove unused files
    
    * update README
    
    * make help system a bit more logical
    
    * remove pushd and popd
    
    * show help when ivalid flags given
    
    * improve handling of invalid commands and options
    
    * unify container names to use dash only
    
    * call ethereum-watcher-local just ethereum-watcher
    
    * add --except option
    
    * add 'wait' command to wait for health checks to finish
    
    * add note about 'wait' to readme
    
    * add 'shell' command
    
    * add 'shell' command to readme
    
    * add health check to cassandra image
    
    * add health check to mysql
    
    * add healthcheck to data-union-server
    
    * add health check for nginx
    
    * add health check to redis
    
    * remove debug logging
    
    * tweak healthcheck intervals to be more frequent
    
    * add convenience --wait flag for start command
    
    * Remove unused variable
    
    * Reformat case/esac for better readability
    
    * add 'update' command
    
    Co-authored-by: Kare Nuorteva <kare.nuorteva@streamr.com>

--- streamr-docker-dev/bin.sh
+++ streamr-docker-dev/bin.sh
@@ -3,80 +3,84 @@
 ORIG_FILENAME="$(readlink "$0" -f)"
 ORIG_DIRNAME=$(dirname "$ORIG_FILENAME")
 ROOT_DIR="$ORIG_DIRNAME/.."
-ARGUMENTS=$#
+CONTAINER_PREFIX="streamr-dev-"
 
 OPERATION=
 COMMANDS_TO_RUN=()
 
-FIRST_START_ARGUMENTS=""
 SERVICES=""
+EXCEPT_SERVICES=""
 FLAGS=""
 DETACHED=1
-REMOVE_DATA=0
-KILL_ALL=0
-ALL=0
 DRY_RUN=0
 FOLLOW=0
-HELP=0
+WAIT=0
+WAIT_TIMEOUT=300     # seconds
 
-SERVICE_COMBINATIONS=(
-""
-"cassandra init_keyspace mysql redis smtp nginx tracker broker-node-storage-1 broker-node-no-storage-1 broker-node-no-storage-2 community-product"
-"cassandra init_keyspace mysql redis smtp nginx broker-node-storage-1 broker-node-no-storage-1 broker-node-no-storage-2"
-"cassandra init_keyspace mysql redis smtp nginx tracker engine-and-editor"
-"cassandra init_keyspace mysql redis smtp nginx "
-"cassandra init_keyspace mysql redis smtp nginx tracker broker-node-storage-1 broker-node-no-storage-1 broker-node-no-storage-2 engine-and-editor"
-"cassandra init_keyspace mysql redis smtp nginx tracker broker-node-storage-1 broker-node-no-storage-1 broker-node-no-storage-2 engine-and-editor platform"
-)
+# Execute all commands from the root dir of streamr-docker-dev
+cd "$ROOT_DIR" || exit 1
 
 help() {
-    $ORIG_DIRNAME/help_scripts.sh
-    exit
-}
-
-check_services_from_arguments() {
-    if [ "$SERVICES" == "" ] && [ $ALL == 0 ]; then
-        echo No services specified. Use option --all if you want to "$OPERATION" all services.
-        exit
-    fi
+    "$ORIG_DIRNAME/help_scripts.sh" $SERVICES
 }
 
 start() {
     ip_lines=$(ifconfig | grep -c 10.200.10.1)
     if [ "$ip_lines" -eq "0" ]; then
-       echo "WARNING: bind-ip is not set! Setting it now."
-       bind_ip
+        COMMANDS_TO_RUN+=("echo Binding the internal IP address to the loopback interface.")
+        COMMANDS_TO_RUN+=("echo This requires sudo privileges, so please provide your password if requested")
+        COMMANDS_TO_RUN+=("sudo ifconfig lo0 alias 10.200.10.1/24")
     fi
-    check_services_from_arguments
     [[ $DETACHED == 1 ]] && FLAGS+=" -d"
-    [[ $SERVICES == "" ]] && msg="Starting all" || msg="Starting$SERVICES"
+    [[ $SERVICES == "" ]] && msg="Starting all" || msg="Starting $SERVICES"
     COMMANDS_TO_RUN+=("echo $msg")
+    COMMANDS_TO_RUN+=("docker-compose up $FLAGS $SERVICES")
 
-    COMMANDS_TO_RUN+=("docker-compose$FIRST_START_ARGUMENTS up$FLAGS$SERVICES")
+    # "--except" feature is implemented by starting all, then stopping the unwanted services.
+    # Bit of a hack but docker-compose doesn't provide a better direct way.
+    if [[ $EXCEPT_SERVICES != "" ]]; then
+        COMMANDS_TO_RUN+=("docker-compose kill $EXCEPT_SERVICES")
+        COMMANDS_TO_RUN+=("docker-compose rm -f $EXCEPT_SERVICES")
+    fi
+
+    if [ $WAIT == 1 ]; then
+        COMMANDS_TO_RUN+=("wait")
+    fi
 }
 
 stop() {
-    check_services_from_arguments
-    [[ $KILL_ALL == 1 ]] && ARGUMENTS="" || ARGUMENTS=$SERVICES
-
-    [[ $ARGUMENTS == "" ]] && msg="Stopping all" || msg="Stopping$ARGUMENTS"
+    [[ $SERVICES == "" ]] && msg="Stopping all" || msg="Stopping $SERVICES"
     COMMANDS_TO_RUN+=("echo $msg")
-
-    COMMANDS_TO_RUN+=("docker-compose kill$ARGUMENTS")
-    COMMANDS_TO_RUN+=("docker-compose rm -f$ARGUMENTS")
-
-    if [ $REMOVE_DATA == 1 ]; then
-        COMMANDS_TO_RUN+=("echo Deleting persistent data")
-        COMMANDS_TO_RUN+=("rm -rf ./data")
-    fi
+    COMMANDS_TO_RUN+=("docker-compose kill $SERVICES")
+    COMMANDS_TO_RUN+=("docker-compose rm -f $SERVICES")
 }
 
 restart() {
     stop && COMMANDS_TO_RUN+=("printf \n") && start
 }
 
+wait() {
+    # Warning: depends on docker-compose ps output, could break easily
+    echo "Waiting for pending health checks to pass (timeout: $WAIT_TIMEOUT sec)..."
+    declare -i time_waited
+    while [[ $time_waited -lt $WAIT_TIMEOUT ]]; do
+        # Are there lines that contain "health" (have health checks) but are not "healthy"?
+        if docker-compose ps | grep health | grep -q -v healthy; then
+            sleep 10s
+            time_waited=$((time_waited+10))
+        else
+            break
+        fi
+    done
+
+    if [[ $time_waited -ge $WAIT_TIMEOUT ]]; then
+        echo "ERROR: Timed out waiting for health checks to pass. (Timeout: $WAIT_TIMEOUT sec)"
+        exit 1
+    fi
+}
+
 ps() {
-    COMMANDS_TO_RUN+=("docker-compose ps$SERVICES")
+    COMMANDS_TO_RUN+=("docker-compose ps $SERVICES")
 }
 
 log() {
@@ -84,222 +88,121 @@ log() {
     if [ $FOLLOW == 1 ];then
         FLAGS+=" -f"
     fi
-    COMMANDS_TO_RUN+=("docker-compose logs$FLAGS$SERVICES")
+    COMMANDS_TO_RUN+=("docker-compose logs $FLAGS $SERVICES")
 }
 
-bind_ip() {
-    COMMANDS_TO_RUN+=("echo Binding the ip addresses requires to be run as sudo and therefore a sudo user's password is required")
-    COMMANDS_TO_RUN+=("sudo ifconfig lo0 alias 10.200.10.1/24")
+shell() {
+    # Assumes standardized container names that begin with $CONTAINER_PREFIX
+    COMMANDS_TO_RUN+=("docker exec -ti $CONTAINER_PREFIX$SERVICES /bin/sh")
 }
 
 pull() {
     # Pull latest images define on docker compose
-    # --all for all services, Service name for specific service
-    check_services_from_arguments
     COMMANDS_TO_RUN+=("docker-compose pull $SERVICES")
 }
 
-clean() {
-    COMMANDS_TO_RUN+=("echo Stopping all")
-    COMMANDS_TO_RUN+=("docker-compose kill")
+update() {
+    # git pull latest version
+    COMMANDS_TO_RUN+=("git pull")
+}
+
+wipe() {
+    stop
+    COMMANDS_TO_RUN+=("echo Wiping persistent data of services")
+    COMMANDS_TO_RUN+=("rm -rf ./data")
+}
+
+factory-reset() {
+    wipe
+    COMMANDS_TO_RUN+=("echo Pruning docker images. This may take a while...")
     COMMANDS_TO_RUN+=("docker system prune --all --force --volumes")
 }
 
-interactive() {
-    PS3="Operation: [type or select from list] > "
-    options=(
-        "start"
-        "stop"
-        "restart"
-        "ps"
-        "log"
-        "bind-ip"
-        "pull"
-        "help"
-    )
-    select opt in "${options[@]}"; do
-        case $REPLY in
-            1 )
-                OPERATION="start"
-                break
-                ;;
-            2 )
-                OPERATION="stop"
-                break
-                ;;
-            3 )
-                OPERATION="restart"
-                break
-                ;;
-            4 )
-                OPERATION="ps"
-                break
-                ;;
-            5 )
-                OPERATION="log"
-                break
-                ;;
-            6 )
-                OPERATION="bind-ip"
-                break
-                ;;
-            7 )
-                OPERATION="pull"
-                break
-                ;;
-            8 )
-                help
-                break
-                ;;
-            * )
-                OPERATION=$REPLY
-                if [[ ! " ${options[@]} " =~ " ${REPLY} " ]]; then
-                    printf "Invalid operation"
-                    exit
-                fi
-                break
-                ;;
+OPERATION=$1
+shift
+
+# Read arguments & options
+while [ $# -gt 0 ]; do # if there are arguments
+    if [[ "$1" = -* ]]; then
+        case $1 in
+        --except )
+            EXCEPT_SERVICES+="$2 "
+            shift # skip over the next arg, which was already consumed above
+            ;;
+        --wait )
+            WAIT=1
+           ;;
+        --timeout )
+            WAIT_TIMEOUT=$2
+            shift # skip over the next arg, which was already consumed above
+            ;;
+        -f | --follow )
+            FOLLOW=1
+            ;;
+        --dry-run )
+            DRY_RUN=1
+            ;;
+        --attached )
+            DETACHED=0
+            ;;
+        * )
+            help
+            echo "ERROR: Invalid option: $1"
+            exit 1
+            ;;
         esac
-    done
-    printf "\n"
-
-    response=
-
-    ask_for_services=(
-        "start"
-        "stop"
-        "restart"
-        "log"
-    )
-    if [[ " ${ask_for_services[@]} " =~ " ${OPERATION} " ]]; then
-        PS3="Services: [type or select bundle from list] > "
-        options=(
-            "Tracker + Broker node + base services"
-            "Broker node + base services"
-            "Tracker + EE + base services"
-            "3rd party + base services"
-            "back-end services + base services"
-            "entire stack"
-        )
-        select opt in "${options[@]}"
-        do
-            temp=""
-            case $REPLY in
-                1 | 2 | 3 | 5 | 6 )         temp=${SERVICE_COMBINATIONS[$REPLY]}
-                                    ;;
-                4 )                 FIRST_START_ARGUMENTS+=" -f docker-compose.yml"
-                                    ;;
-                * )                 temp=$REPLY
-                                    ;;
-            esac
-            SERVICES=" $temp"
-            break
-        done
-
-        if [ "$OPERATION" == "stop" ] || [ "$OPERATION" == "restart" ]; then
-            if [ $ALL == 0 ]; then
-                printf "Stop other services also? [y/n] > "
-                read response
-                if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-                    KILL_ALL=1
-                fi
-            fi
-            printf "\n"
-
-            printf "Remove persistent data? [y/n] > "
-            read response
-            if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-                REMOVE_DATA=1
-            fi
-            printf "\n"
-        elif [ "$OPERATION" == "log" ]; then
-            printf "Follow log? [y/n] > "
-            read response
-            if [ "$response" == "Y" ] || [ "$response" == "y" ]; then
-                FOLLOW=1
-            fi
-        fi
-    elif [ "$OPERATION" == "bind-ip" ]; then
-        bind_ip
-    elif [ "$OPERATION" == "help" ]; then
-        help
-    elif [ "$OPERATION" == "pull" ]; then
-        ALL=1
-        pull
+    else
+        SERVICES+="$1 "
     fi
-}
-
-if [ $# == 0 ]; then
-    interactive
-else
-    OPERATION=$1
     shift
+done
 
-    # Read arguments & options
-    while [ $# -gt 0 ]; do # if there are arguments
-        if [[ "$1" = -* ]]; then
-            case $1 in
-                -h | --help )               HELP=1
-                                            ;;
-                -a | --all )                ALL=1
-                                            ;;
-                -r | --remove-data )        REMOVE_DATA=1
-                                            ;;
-                -k | --kill-all )           KILL_ALL=1
-                                            ;;
-                -f | --follow )             FOLLOW=1
-                                            ;;
-                --dry-run )                 DRY_RUN=1
-                                            ;;
-                --attached )                DETACHED=0
-                                            ;;
-                * )                         echo "Invalid option: $1"
-                                            exit
-                                            ;;
-            esac
-        else
-            case $1 in
-                1 | 2 | 3 | 5 | 6)  SERVICES+=" ${SERVICE_COMBINATIONS[$1]}"
-                            ;;
-                4 )         FIRST_START_ARGUMENTS+=" -f docker-compose.yml"
-                            SERVICES+=" "
-                            ;;
-                * )         SERVICES+=" $1"
-                            ;;
-            esac
-        fi
-        shift
-    done
-fi
-
-if [ $HELP == 1 ]; then
-    $ORIG_DIRNAME/help_scripts.sh "$OPERATION"
-    exit
-fi
-
+# Populate COMMANDS_TO_RUN by executing the relevant method
 case $OPERATION in
-    help | -h | --help )            help
-                                    ;;
-    start )                         start
-                                    ;;
-    stop )                          stop
-                                    ;;
-    restart )                       restart
-                                    ;;
-    ps )                            ps
-                                    ;;
-    log )                           log
-                                    ;;
-    pull )                          pull
-                                    ;;
-    "bind-ip" )                     bind_ip
-                                    ;;
-    clean)                          clean
-                                    ;;
+"" | help )
+    help
+    ;;
+start )
+    start
+    ;;
+stop )
+    stop
+    ;;
+restart )
+    restart
+    ;;
+wait )
+    wait
+    ;;
+ps )
+    ps
+    ;;
+log )
+    log
+    ;;
+shell )
+    shell
+    ;;
+pull )
+    pull
+    ;;
+update )
+    update
+    ;;
+wipe )
+    wipe
+    ;;
+factory-reset )
+    factory-reset
+    ;;
+* )
+    help
+    echo "ERROR: Invalid command: $OPERATION"
+    exit 1
+    ;;
 esac
 
-
-pushd $ROOT_DIR > /dev/null
+# Run or dry-run COMMANDS_TO_RUN
 for command in "${COMMANDS_TO_RUN[@]}"
 do
     if [ $DRY_RUN == 1 ]; then
@@ -308,5 +211,3 @@ do
         $command
     fi
 done
-popd > /dev/null
-
--- streamr-docker-dev/help_scripts.sh
+++ streamr-docker-dev/help_scripts.sh
@@ -5,91 +5,104 @@ echo "
 streamr-docker-dev – Streamr Docker Developer Environment.
 
 Usage: streamr-docker-dev [<command> [options] [--] <service>...]
-    streamr-docker-dev
-    streamr-docker-dev start --all
-    streamr-docker-dev stop -r broker data-api
-    streamr-docker-dev restart 1
 
 Commands:
     help                show this screen
     start               start services
     stop                stop services
-    restart             stop and start services 
+    restart             stop and start services
+    wait                wait for all health checks to pass
     ps                  list docker containers
-    bind-ip             attach (unused) IP address 10.200.10.1 to network interface lo0 on macOS details
     log                 show logs
-    pull                pulls images
-    clean               removes images and generated files
+    shell               shell into a container
+    pull                pulls latest versions of images
+    update              updates streamr-docker-dev by doing git pull
+    wipe                wipes the data persisted by all services
+    factory-reset       removes images and generated files
+
+
+Examples:
+    streamr-docker-dev start
+    streamr-docker-dev stop tracker
+    streamr-docker-dev start --except tracker --wait
+    streamr-docker-dev log -f tracker
+    streamr-docker-dev shell tracker
+    streamr-docker-dev pull
 
 Options:
-    -h --help           command help
-    --dry-run   
+    --dry-run           echo commands instead of executing them
 
-Show command-spesific options:
-	streamr-docker-dev <command> -h               
+Show command-specific options:
+	streamr-docker-dev help <command>
 "
 }
 
 start_help() {
 echo "
-Starts the given services
+Starts the given services, or all services if none are specified.
 
-Usage: streamr-docker-dev start [options] [--] <service>...
-    streamr-docker-dev start --all
-    streamr-docker-dev start broker data-api
+Usage: streamr-docker-dev start [--] <service>...
+
+Examples:
+    streamr-docker-dev start
+    streamr-docker-dev start tracker ganache
+    streamr-docker-dev start --except tracker --wait
 
 Options:
-    -a --all           apply to all services         
+    --except [service]      start services except the one given
+    --wait                  after starting, wait for services to become healthy (same as 'wait' command)
+    --attached              start in attached mode
 "
 }
 
 stop_help() {
 echo "
-Stops the given services
+Stops the given services, or all services if none are specified.
 
 Usage: streamr-docker-dev stop [options] [--] <service>...
-    streamr-docker-dev stop --all
-    streamr-docker-dev stop broker data-api
-    streamr-docker-dev stop -r broker data-api
 
-Options:
-    -a --all        	    apply to all services
-    -r --remove-data    	remove persistent data      	
+Examples:
+    streamr-docker-dev stop
+    streamr-docker-dev stop tracker ganache
 "
 }
 
 restart_help() {
 echo "
-Restarts the given services
+Restarts the given services, or all services if none are specified
 
 Usage: streamr-docker-dev restart [options] [--] <service>...
-    streamr-docker-dev restart --all
-    streamr-docker-dev restart broker data-api
-    streamr-docker-dev restart -r -k broker data-api
+
+Examples:
+    streamr-docker-dev restart
+    streamr-docker-dev restart tracker ganache
+"
+}
+
+wait_help() {
+echo "
+Waits until pending health checks pass. Ignores services without health checks.
+
+Usage: streamr-docker-dev wait [options]
+
+Examples:
+    streamr-docker-dev wait
+    streamr-docker-dev wait --timeout 300
 
 Options:
-    -a --all        	    apply both stop and start to all services
-    -r --remove-data    	remove persistent data
-    -k --kill-all			apply stop to all services (does nothing with --all)      	
+    --timeout [sec]      Sets the maximum time to wait. Default: 300 sec
 "
 }
 
 ps_help() {
 echo "
-Shows current services. If no services given, shows all
+Shows currently running services. If no services given, shows all
 
 Usage: streamr-docker-dev ps [<service>...]
+
+Examples:
     streamr-docker-dev ps
-    streamr-docker-dev ps broker data-api
-"
-}
-
-bind_ip_help() {
-echo "
-Attach (unused) IP address 10.200.10.1 to network interface lo0 on macOS details
-More info in https://docs.docker.com/docker-for-mac/networking/#use-cases-and-workarounds
-
-Usage: streamr-docker-dev bind-ip
+    streamr-docker-dev ps tracker
 "
 }
 
@@ -98,58 +111,103 @@ echo "
 Shows the logs of the given services
 
 Usage: streamr-docker-dev log [[options] [--] <service>...]
+
+Examples:
     streamr-docker-dev log
-    streamr-docker-dev log broker data-api
-    streamr-docker-dev log -f broker data-api
+    streamr-docker-dev log tracker ganache
+    streamr-docker-dev log -f tracker ganache
 
 Options:
-    -f --follow        	    follow log in realtime     	
+    -f --follow        	    follow log in realtime
+"
+}
+
+shell_help() {
+echo "
+Opens an interactive shell into the target container.
+
+Usage: streamr-docker-dev shell <service>
+
+Examples:
+    streamr-docker-dev shell tracker
 "
 }
 
 pull_help() {
 echo "
-Pulls images defined in docker-compose files
+Pulls images defined in docker-compose files. If no services are given, pulls all of them.
 
 Usage: streamr-docker-dev pull [ [--] <service>...]
-    streamr-docker-dev pull --all
-    streamr-docker-dev pull broker ganache
+
+Examples:
+    streamr-docker-dev pull
+    streamr-docker-dev pull tracker ganache
 "
 }
 
-clean_help() {
-    echo "
-Clean removes all files generated by Docker
+update_help() {
+echo "
+Updates streamr-docker-dev to the latest version by doing a "git pull" in the streamr-docker-dev directory.
 
-Usage: streamr-docker-dev clean
+Usage: streamr-docker-dev update
+"
+}
+
+wipe_help() {
+echo "
+Wipes the data persisted by all services.
+
+Usage: streamr-docker-dev wipe
+"
+}
+
+factory-reset_help() {
+    echo "
+Resets the environment by removing all persisted data and all related Docker images.
+
+Usage: streamr-docker-dev factory-reset
 "
 }
 
 case $1 in
-    "start" )
-        start_help
-        ;;
-    "stop" )
-        stop_help
-        ;;
-    "restart" )
-        restart_help
-        ;;
-    "ps" )
-        ps_help
-        ;;
-    "bind-ip" )
-        bind_ip_help
-        ;;
-    "log" )
-        log_help
-        ;;
-    "pull" )
-        pull_help
-        ;;
-    "clean" )
-        clean_help
-        ;;
-    * ) common_help
-        ;;
+"" )
+    common_help
+    ;;
+"start" )
+    start_help
+    ;;
+"stop" )
+    stop_help
+    ;;
+"restart" )
+    restart_help
+    ;;
+"wait" )
+    wait_help
+    ;;
+"ps" )
+    ps_help
+    ;;
+"log" )
+    log_help
+    ;;
+"shell" )
+    shell_help
+    ;;
+"pull" )
+    pull_help
+    ;;
+"update" )
+    update_help
+    ;;
+"wipe" )
+    wipe_help
+    ;;
+"factory-reset" )
+    factory-reset_help
+    ;;
+* ) common_help
+    echo "ERROR: No help available for invalid command: $1"
+    exit 1
+    ;;
 esac
