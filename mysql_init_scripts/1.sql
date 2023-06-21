bf2c671b44bd80e76a665e1cca0d4b3d25da4589
Henri Pihkala <henri@streamr.network>
2023-06-21 17:13:39 +0200

    remove unused services

--- mysql_init_scripts/1.sql
+++
@@ -1,3 +0,0 @@
--- This script is run 1st (hence the name)
-CREATE DATABASE IF NOT EXISTS core_dev CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS core_test CHARACTER SET utf8 COLLATE utf8_general_ci;
